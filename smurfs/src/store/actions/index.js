0be4983d4ea33343f0219e4b856d4e076fec9cee
Bryan Adams <bryanadams@Bryans-MacBook-Air.local>
2020-10-09 10:49:12 -0700

    completed mvp

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -11,7 +11,7 @@ export const POST_FAILURE = "POST_FAILURE";
 export const fetchSmurfs = () => (dispatch) => {
   dispatch({ type: FETCH_START });
   axios
-    .get("http://localhost:3333/smurfs")
+    .get("http://localhost:3334/smurfs")
     .then((res) => dispatch({ type: FETCH_SUCCESS, payload: res.data }))
     .catch((err) => dispatch({ type: FETCH_FAILURE, payload: err.response }));
 };
@@ -19,7 +19,7 @@ export const fetchSmurfs = () => (dispatch) => {
 export const postSmurf = (smurf) => (dispatch) => {
   dispatch({ type: POST_START });
   axios
-    .post("http://localhost:3333/smurfs", smurf)
+    .post("http://localhost:3334/smurfs", smurf)
     .then((res) => dispatch({ type: POST_SUCCESS, payload: res.data }))
     .catch((err) => dispatch({ type: POST_FAILURE, payload: err.response }));
 };
